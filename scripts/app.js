6152b95242439f3bce6835cf52d4b422fd2aafc9
Ikraam <IbnDaanis@Gmail.com>
2021-01-06 21:32:42 +0000

    Added updateScoreboard function and data attributes to each cell

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,37 @@
 const gameboard = (() => {
-  const scoreboard = []
+  let scoreboard = ['', '', '', '', '', '', '', '', '']
   const gameboardEl = document.querySelector('#gameboardEl')
+  const winningScores = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
 
   const createGame = () => {
     for (let i = 0; i < 9; i++) {
       const cell = document.createElement('div')
       cell.classList.add('cell')
-      scoreboard.push(cell.textContent)
+      cell.setAttribute('data-index', i)
+      console.log(scoreboard)
+      cell.textContent = scoreboard[i]
       gameboardEl.appendChild(cell)
     }
+  }
+  const updateScoreboard = scores => {
+    scoreboard = scores
   }
   return {
     createGame,
     scoreboard,
+    updateScoreboard,
+    winningScores,
   }
 })()
 
+gameboard.updateScoreboard(['', '', '', '', '', 'X', 'X', ''])
 gameboard.createGame()
