3118af02d048b40dfe6e3deada17c00042624b26
Ikraam <IbnDaanis@Gmail.com>
2021-01-07 15:20:33 +0000

    Changed styling, added check winner function

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,8 @@
 const gameboard = (() => {
   let scoreboard = ['', '', '', '', '', '', '', '', '']
+  let scoreX = 0
+  let scoreO = 0
+  let player = false
   const gameboardEl = document.querySelector('#gameboardEl')
   const winningScores = [
     [0, 1, 2],
@@ -13,25 +16,60 @@ const gameboard = (() => {
   ]
 
   const createGame = () => {
+    gameboardEl.innerHTML = ''
     for (let i = 0; i < 9; i++) {
       const cell = document.createElement('div')
       cell.classList.add('cell')
       cell.setAttribute('data-index', i)
-      console.log(scoreboard)
       cell.textContent = scoreboard[i]
       gameboardEl.appendChild(cell)
     }
+    gameboardEl.style.opacity = '1'
+    playerMove()
   }
-  const updateScoreboard = scores => {
-    scoreboard = scores
+
+  const checkWinner = () => {
+    winningScores.forEach(win => {
+      if (scoreX === 3 || scoreO === 3) return
+      scoreX = 0
+      scoreO = 0
+      win.forEach(cell => {
+        if (scoreboard[cell] === 'X') {
+          scoreX++
+        } else if (scoreboard[cell] === 'O') {
+          scoreO++
+        }
+      })
+    })
+    return (scoreX === 3 && 'X won') || (scoreO === 3 && 'O won') || 'Draw'
+  }
+  const updateScoreboard = (index, player) => {
+    if (scoreboard[index]) return
+    scoreboard[index] = player
+    createGame()
+  }
+
+  const playerMove = () => {
+    document.querySelectorAll('.cell').forEach(cell => {
+      cell.addEventListener('click', e => {
+        if (scoreX === 3 || scoreO === 3) return
+        const cellNumber = e.target.dataset.index
+
+        player = !player
+        updateScoreboard(cellNumber, player ? 'O' : 'X')
+        console.log(checkWinner())
+      })
+    })
   }
   return {
     createGame,
     scoreboard,
     updateScoreboard,
     winningScores,
+    checkWinner,
+    playerMove,
   }
 })()
 
-gameboard.updateScoreboard(['', '', '', '', '', 'X', 'X', ''])
 gameboard.createGame()
+gameboard.playerMove()
